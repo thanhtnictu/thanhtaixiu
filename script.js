25ef319d97a2340ee8afcb5cb7b7a6013d4081a5
thanhtnictu <80505176+thanhtnictu@users.noreply.github.com>
2022-01-28 19:18:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let images = ["dice-01.svg",
+"dice-02.svg",
+"dice-03.svg",
+"dice-04.svg",
+"dice-05.svg",
+"dice-06.svg"];
+let dice = document.querySelectorAll("img");
+
+function roll(){
+    dice.forEach(function(die){
+        die.classList.add("shake");
+    });
+    setTimeout(function(){
+        dice.forEach(function(die){
+            die.classList.remove("shake");
+        });
+        let dieOneValue = Math.floor(Math.random()*6);
+        let dieTwoValue = Math.floor(Math.random()*6);
+        let dieTthreeValue = Math.floor(Math.random()*6);
+        console.log(dieOneValue,dieTwoValue);
+        document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
+        document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
+        document.querySelector("#die-3").setAttribute("src", images[dieTthreeValue]);
+        document.querySelector("#total").innerHTML = `Và đó là:  ${(dieOneValue + dieTwoValue + dieTthreeValue + 3) < 10 ? 'Tài' : 'Xỉu'}`;
+
+    },
+    1000
+    );
+}
+roll();
